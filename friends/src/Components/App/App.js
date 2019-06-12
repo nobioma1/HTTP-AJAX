777c93f07559fa0c32a3becf2dd1348f12725ef0
Noble Obioma <nobioma1@gmail.com>
2019-06-12 13:25:23 +0100

    Get data using axios when component mounts and update the friends array in state, then pass into friendsList component

--- friends/src/Components/App/App.js
+++ friends/src/Components/App/App.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
+import FriendsList from '../FriendsList';
+import Header from '../Header';
+
+const AppContainer = styled.div`
+  width: 100%;
+`;
+
+const MainContainer = styled.div`
+  margin-top: 100px;
+  display: flex;
+  justify-content: center;
+`;
+
+class App extends Component {
+  state = {
+    friends: []
+  };
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/friends')
+      .then(res => this.setState({ friends: res.data }))
+      .catch(err => this.setState({ err: err.message }));
+  }
+
+  render() {
+    return (
+      <AppContainer>
+        <Header />
+        <MainContainer>
+          <FriendsList friends={this.state.friends} />
+        </MainContainer>
+      </AppContainer>
+    );
+  }
 }
 
 export default App;
