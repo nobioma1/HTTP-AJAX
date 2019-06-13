aa4d672bf904bd82899d0f6dfbfc8a5c9452b715
Noble Obioma <nobioma1@gmail.com>
2019-06-13 13:01:15 +0100

    Add update and delete buttons and set onclick on delete button to invoke delete friend method on props

--- friends/src/Components/FriendsList.js
+++ friends/src/Components/FriendsList.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { FaPlus } from 'react-icons/fa';
+import { FaPlus, FaRegEdit, FaTrashAlt } from 'react-icons/fa';
 
 import { ContainerCard, ContainerCardHeader } from './sharedStyle';
 
 const FriendDiv = styled.li`
   border-bottom: 1px solid #899878;
+
   &:last-child {
     border-bottom: none;
+  }
+
+  p {
+    margin: 5px 0;
   }
 
   span {
@@ -17,19 +22,32 @@ const FriendDiv = styled.li`
   }
 `;
 
-const FriendList = props => {
-  const goToAddFriend = () => props.history.push('/add_friend');
+const ButtonsContainer = styled.div`
+  width: 100%;
+  margin: 5px 0;
 
+  button {
+    padding: 5px;
+    margin: 5px;
+    border: 1px solid gray;
+    font-size: 15px;
+    border-radius: 3px;
+  }
+`;
+
+const goToAddFriend = history => history.push('/add_friend');
+
+const FriendList = ({ friends, history, deleteFriend }) => {
   return (
     <ContainerCard>
       <ContainerCardHeader>
         <h3>My Friends</h3>
-        <button onClick={goToAddFriend}>
+        <button onClick={() => goToAddFriend(history)}>
           <FaPlus /> Add New Friend
         </button>
       </ContainerCardHeader>
       <ol>
-        {props.friends.map(friend => (
+        {friends.map(friend => (
           <FriendDiv key={friend.id}>
             <p>
               Name: <span>{friend.name}</span>
@@ -40,6 +58,14 @@ const FriendList = props => {
             <p>
               email: <span>{friend.email}</span>
             </p>
+            <ButtonsContainer>
+              <button>
+                <FaRegEdit /> Edit
+              </button>
+              <button onClick={() => deleteFriend(friend.id)}>
+                <FaTrashAlt /> Delete
+              </button>
+            </ButtonsContainer>
           </FriendDiv>
         ))}
       </ol>
